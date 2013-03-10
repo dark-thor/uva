4b1b949a07822ea6f09a07a5698dc702fe3dfd6b
gimley <axegimley@gmail.com>
2013-03-09 20:04:30 -0800

    105 Skyline Problem

--- skyline.cc
+++ skyline.cc
@@ -5,16 +5,18 @@ using namespace std;
 unsigned height[10001];
 int main()
 {
-  unsigned l,h,r,i=0,max=0;
+  unsigned l,h,r,i=0,min=0,max=0;
   while (cin>>l>>h>>r) {
     if (max < r)
       max = r;
+    if (!min)
+      min = l;
     for (i=l; i<r; i++)
       if (height[i]<h)
         height[i] = h;
   }
 
-  for(i=1; i<max; i++)
+  for(i=min; i<max; i++)
     if (height[i] != height[i-1])
       cout<<i<<' '<<height[i]<<' ';
   cout<<max<<' '<<'0'<<endl;
